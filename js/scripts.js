09ccbe4e7b04e150fd74cdd1555cb5235e03ced1
Stephen Burden <spburden@hotmail.com>
2016-08-26 11:28:00 -0700

    add header with an to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,31 @@
-// Pizza.prototype.calculateNumberofToppings = function () {
-//   return this.numberOfToppings = this.toppings.length;
-// };
-//
-// Pizza.prototype.calculatePrice = function () {
-//   return this.totalPrice = (this.numberOfToppings * (this.size/10)) + this.size;
-// };
-
 // BUSINESS LOGIC
 function PizzaOrder(name, phone, size) {
   this.name = name;
   this.phone = phone;
   this.size = size;
   this.toppings = [];
-  //this.numberOfToppings = 0;
+  this.pizzaPrice = 0;
   this.totalPrice = 0;
 };
 
-// PizzaOrder.prototype.insertTypeOfTopping = function(topping) {
-//   this.toppings.push(topping);
+// function Pizza(size) {
+//   this.size = size;
+//   this.toppings = [];
+//   this.pizzaPrice = 0;
+// };
+//
+// PizzaOrder.prototype.calculatePizzaPrice = function() {
+//   var numberOfToppings = this.toppings.length;
+//   return this.totalPrice = (numberOfToppings * (this.size/10)) + this.size;
 // };
 
+
 PizzaOrder.prototype.calculatePrice = function() {
-  //var total = 0;
   var numberOfToppings = this.toppings.length;
   return this.totalPrice = (numberOfToppings * (this.size/10)) + this.size;
 };
+
+
 
   // USER INTERFACE LOGIC
   $(function() {
@@ -37,17 +38,12 @@ PizzaOrder.prototype.calculatePrice = function() {
 
 
    var newPizzaOrder = new PizzaOrder(customersName, phoneNumber, sizeOfPizza);
-   //debugger;
+
    $('#toppings :checked').each(function() {
-     //alert($(this).val());
-      //this.toppings.push($(this).val());
-      //newPizzaOrder.insertTypeOfTopping($(this).val())
       newPizzaOrder.toppings.push($(this).val());
     });
 
-    //newPizzaOrder.calculatePrice();
     alert(newPizzaOrder.calculatePrice());
-    //console.log(this.newPizzaOrder);
 
     });
   });
