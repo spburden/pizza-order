92324f79971a85a7b01cc5631ef76514956f1156
Stephen Burden <spburden@hotmail.com>
2016-08-26 14:09:11 -0700

    output a list with pizza size and price

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,25 @@
 // BUSINESS LOGIC
-function PizzaOrder(name, phone, size) {
-  this.name = name;
-  this.phone = phone;
-  this.size = size;
-  this.toppings = [];
-  this.pizzaPrice = 0;
-  this.totalPrice = 0;
-};
-
 function Pizza(size) {
   this.size = size;
   this.toppings = [];
   this.pizzaPrice = 0;
+};
+
+function CustomerInfo(name, phone) {
+  this.name = name;
+  this.phone = phone;
+}
+
+Pizza.prototype.pizzaSizeToString = function() {
+  var pizzaSize = "";
+  if (Pizza.size === 10) {
+    pizzaSize = "Small Pizza"
+  } else if (Pizza.size === 15) {
+    pizzaSize = "Medium Pizza"
+  } else {
+    pizzaSize = "Large Pizza"
+  }
+  return pizzaSize;
 };
 
 Pizza.prototype.calculatePizzaPrice = function() {
@@ -31,20 +39,28 @@ Pizza.prototype.calculatePizzaPrice = function() {
 // USER INTERFACE LOGIC
 $(function() {
    $("form").submit(function(event) {
-   event.preventDefault();
+     event.preventDefault();
 
-   var customersName = $("#customersName").val();
-   var phoneNumber = $("#phoneNumber").val();
-   var sizeOfPizza = parseInt($("#sizeOfPizza input[type='radio'][name='pizzaSize']:checked").val());
+     var customersName = $("#customersName").val();
+     var phoneNumber = $("#phoneNumber").val();
+     var sizeOfPizza = parseInt($("#sizeOfPizza input[type='radio'][name='pizzaSize']:checked").val());
+
+     var newPizza = new Pizza(sizeOfPizza);
+
+     var newCustomerInfo = new CustomerInfo(customersName, phoneNumber);
+
+     $('#toppings :checked').each(function() {
+        newPizza.toppings.push($(this).val());
+      });
+
+      alert("Pizza price: $" + newPizza.calculatePizzaPrice());
+
+      $(".customersName").text(newCustomerInfo.name);
+      $("#orderList").append("<li>" + newPizza.pizzaSizeToString() + " : $"+ newPizza.pizzaPrice +  "</li>");
+      $("#orderList").show();
 
 
-   var newPizzaOrder = new PizzaOrder(customersName, phoneNumber, sizeOfPizza);
 
-   $('#toppings :checked').each(function() {
-      newPizzaOrder.toppings.push($(this).val());
-    });
-
-    alert(newPizzaOrder.calculatePrice());
 
   });
 });
