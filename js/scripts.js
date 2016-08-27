8282822ae2ae92b4b88414a6154097ff504425fa
Stephen Burden <spburden@hotmail.com>
2016-08-26 17:03:34 -0700

    remove white space and hide name input

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,10 @@ function Pizza(size) {
   this.toppings = [];
   this.pizzaPrice = 0;
 };
-
 function CustomerInfo(name, phone) {
   this.name = name;
   this.phone = phone;
-}
-
+};
 Pizza.prototype.pizzaSizeToString = function() {
   var pizzaSize = "";
   if (this.size === 10) {
@@ -21,18 +19,14 @@ Pizza.prototype.pizzaSizeToString = function() {
   }
   return pizzaSize;
 };
-
 Pizza.prototype.calculatePizzaPrice = function() {
   var numberOfToppings = this.toppings.length;
   return this.pizzaPrice = (numberOfToppings * (this.size/10)) + this.size;
 };
-
 Pizza.prototype.calculateOrder = function() {
   var total = 0;
-
   return this.pizzaPrice = (numberOfToppings * (this.size/10)) + this.size;
 };
-
 Pizza.prototype.listToppings = function() {
   var toppings = "";
   if (this.toppings[1] !== undefined){
@@ -45,38 +39,25 @@ Pizza.prototype.listToppings = function() {
   }
   return toppings;
 };
-
-// PizzaOrder.prototype.calculatePrice = function() {
-//   var numberOfToppings = this.toppings.length;
-//   return this.totalPrice = (numberOfToppings * (this.size/10)) + this.size;
-// };
-
-
-
 // USER INTERFACE LOGIC
 $(function() {
    var total = 0;
    $("form").submit(function(event) {
      event.preventDefault();
-
      var customersName = $("#customersName").val();
      var phoneNumber = $("#phoneNumber").val();
      var sizeOfPizza = parseInt($("#sizeOfPizza input[type='radio'][name='pizzaSize']:checked").val());
-
      var newPizza = new Pizza(sizeOfPizza);
-
      var newCustomerInfo = new CustomerInfo(customersName, phoneNumber);
-
      $('#toppings :checked').each(function() {
         newPizza.toppings.push($(this).val());
       });
-
       alert("Pizza price: $" + newPizza.calculatePizzaPrice());
       total+=newPizza.calculatePizzaPrice();
-
       $(".customersName").text(newCustomerInfo.name);
       $("#orderList").append("<li>" + newPizza.pizzaSizeToString() + ": " + newPizza.listToppings() + " : $"+ newPizza.pizzaPrice +  "</li>");
-      $("#orderList").show();
+      $("#checkout").show();
+      $("#customerDetails").hide(); 
    });
    $("#placeOrder").click(function(){
     confirm("Your total is: $" + total);
